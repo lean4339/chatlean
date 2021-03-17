7b2b4602597b43440f1f0708f76593b3330ecd0a
Leandro Ayala <66891357+lean4339@users.noreply.github.com>
2021-03-17 18:49:49 -0300

    primer version de app

---
+++ public/javascripts/chat.js
@@ -0,0 +1,26 @@
+var socket = io("http://localhost:3000",{ 'forceNew': true });
+  var nombre = document.querySelector("#nombre");
+  var form = document.getElementById('form');
+  var input = document.getElementById('input');
+  const mensajes = document.querySelector("#messages");
+  const oldHTML = mensajes.innerHTML
+  socket.on('messages', function(data) {    
+      let mensajies =  data.map(element => {
+        console.log(element.name)
+         return `<li>${element.text} de <strong>${element.name}</strong></li>`
+       });
+       mensajes.innerHTML = `${mensajies}`
+       
+    });
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let mensaje = {
+      name : nombre.value,
+      text: input.value
+    }
+    if (input.value && nombre.value) {
+      socket.emit('new-message', mensaje);
+      input.value = '';
+    }
+  });
